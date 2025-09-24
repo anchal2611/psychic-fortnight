0f68ba171ef1cf79a57ce7d2a71c8d2a121c7e16
Anchal Gupta <anchdel06@gmail.com>
2025-09-24 17:12:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = nextConfig
+};
 
-// ---------------- pages/_app.js ----------------
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+module.exports = nextConfig;
